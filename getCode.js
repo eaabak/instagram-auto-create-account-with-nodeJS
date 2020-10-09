932a76af8a1b44fa4d21c4735f7d512c846612d4
alan4747 <alan47m@gmail.com>
2020-10-09 21:53:47 +0300

    downloaded last chromedriver

--- getCode.js
+++ getCode.js
@@ -6,10 +6,9 @@ const getInstCode = async function (domain, mailName) {
 
     const INST_CODE = 'https://email-fake.com/' + domain + '/' + mailName;
     let code = "";
-    let browser = await new Builder().forBrowser('chrome').build();
     try {
         await browser.get(INST_CODE);
-        await sleep(100000);
+        await sleep(50000);
         code = await browser.findElement(By.xpath("//*[@id='email-table']/div[2]/div[1]/div/h1")).getText();
         console.log(code)
         code = code.replace("is your Instagram code", "");
