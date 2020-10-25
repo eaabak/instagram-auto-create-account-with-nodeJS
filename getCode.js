8e0359ab2206a2ce31c0cbd5f93ef72e86a9b041
alan4747 <alan47m@gmail.com>
2020-10-25 11:33:22 +0300

    Added opening new tab in same browser.

--- getCode.js
+++ getCode.js
@@ -8,6 +8,7 @@ const getInstCode = async function (domain, mailName, browser) {
     let code = "";
     try {
         await browser.get(INST_CODE);
+        await browser.executeScript('window.open("newURL");');
         await sleep(50000);
         code = await browser.findElement(By.xpath("//*[@id='email-table']/div[2]/div[1]/div/h1")).getText();
         console.log(code)
